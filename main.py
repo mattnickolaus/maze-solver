25b22683e41cde1e543128e2a99d7c8727efce48
Matt Nickolaus <97491556+mattnickolaus@users.noreply.github.com>
2025-03-06 16:04:10 -0500

    Update main.py to draw a line to test

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from window import Window
+from graphic import *
 
 def main():
     win = Window(800, 600)
+    p1 = Point(30, 30)
+    p2 = Point(30, 40)
+    line1 = Line(p1, p2)
+    win.draw_line(line1, "red")
+    
     win.wait_for_close()
-
 
 if __name__ == "__main__":
     main()
